6a524d5370e737204fe662539eac97542aa30e33
Mohamed Kamal <mamdkamal861@gmail.com>
2025-08-07 23:42:15 +0300

    undo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-lg border-b border-gray-200 sticky top-0 z-50 mb-25">
+    <nav className="bg-white shadow-lg border-b border-gray-200 sticky top-0 z-50 mb-5">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -54,15 +54,11 @@ export default function Register() {
     onSubmit: handleSubmit,
   });
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 via-indigo-50 to-blue-50 py-12 px-4 sm:px-6 lg:px-8 mt-5">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 via-indigo-50 to-blue-50 py-12 px-4 sm:px-6 lg:px-8 mt-15">
       <div className="max-w-md w-full space-y-8">
         {/* Header */}
         <div className="text-center">
           <div className="flex justify-center">
-            <div className="relative">
-              <CircleCheckBig size={48} className="text-purple-600" />
-              <div className="absolute inset-0 bg-purple-600 rounded-full opacity-20 animate-pulse"></div>
-            </div>
           </div>
           <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
             Join TaskFlow
