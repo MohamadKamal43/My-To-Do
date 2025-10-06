eacb262c38480278e5b191cc9f67f2aad8c047d4
Mohamed Kamal <mamdkamal861@gmail.com>
2025-10-06 23:16:41 +0300

    fixed scroll to top issue

--- src/App.jsx
+++ src/App.jsx
@@ -55,9 +55,14 @@ export default function App() {
   return (
     <div>
       <ScrollToTop smooth 
-       viewBox="0 0 384 512"
-        svgPath="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
-      color="blue"
+       component={"Up"}
+       style={
+      {
+        color:"white",
+        background:"#155dfc",
+        fontSize:"20px"
+      }
+       }
       />
 
       <RouterProvider router={router} />
